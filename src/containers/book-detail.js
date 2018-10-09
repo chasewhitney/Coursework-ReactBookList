2a17c3a6eea3964c224eccfab6ab661b9aaf31cf
Chase Whitney <ChaseEWhitney@gmail.com>
2018-10-09 15:18:41 -0500

    added book-detail container

---
+++ src/containers/book-detail.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class BookDetail extends Component {
+  render() {
+    return (
+      <div>
+      <h1>Book Detail!</h1>
+      </div>
+    )
+  }
+}
