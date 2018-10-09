82e5ed57ca3283a557f5380e13c06572728f4bfd
Chase Whitney <ChaseEWhitney@gmail.com>
2018-10-09 16:16:48 -0500

    done

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -4,13 +4,14 @@ import { connect } from 'react-redux';
 class BookDetail extends Component {
   render() {
     if (!this.props.book) {
-      return <div></div>;
+      return <div>Select a book to get started.</div>;
     }
 
     return (
       <div>
       <h3>Book detail for:</h3>
-        <div>{this.props.book.title}</div>
+        <div>Title: {this.props.book.title}</div>
+        <div>Pages: {this.props.book.pages}</div>
       </div>
     )
   }
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'Javascript: The Good Parts'},
-    { title: 'Harry Potter'},
-    { title: 'The Dark Tower'},
-    { title: 'Eloquent Ruby'},
+    { title: 'Javascript: The Good Parts', pages: 115},
+    { title: 'Harry Potter', pages: 448},
+    { title: 'The Dark Tower', pages: 983},
+    { title: 'Eloquent Ruby', pages: 67},
   ]
 };
