003038d49ca2c273b73e2c7e6a10c1f5b465d134
Chase Whitney <ChaseEWhitney@gmail.com>
2018-10-11 14:21:59 -0500

    updated readme

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -2,6 +2,7 @@
 // this reducer is responsible for.
 // State will default to null, so state is not undefined when app first loaded
 export default function(state = null, action) {
+  console.log('in reducer with:', action);
   switch(action.type) {
     case 'BOOK_SELECTED':
       return action.payload;
